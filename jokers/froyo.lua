3490e55911d161f6922c4c03ed10b41fb305ac3d
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-01-18 18:42:49 -0500

    fix froyo's weird melt timing

--- jokers/froyo.lua
+++ jokers/froyo.lua
@@ -42,20 +42,25 @@ SMODS.Joker {
                     colour = G.C.CHIPS
                 });
             else
-                play_sound('tarot1')
-                card.T.r = -0.2
-                card:juice_up(0.3, 0.4)
-                card.states.drag.is = true
-                card.children.center.pinch.x = true
                 G.E_MANAGER:add_event(Event({
-                    trigger = 'after',
-                    delay = 0.3,
-                    blockable = false,
                     func = function()
-                        G.jokers:remove_card(card)
-                        card:remove()
-                        card = nil
-                        return true;
+                        play_sound('tarot1')
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.3,
+                            blockable = false,
+                            func = function()
+                                G.jokers:remove_card(card)
+                                card:remove()
+                                card = nil
+                                return true; 
+                            end
+                        })) 
+                        return true
                     end
                 }))
                 return {
