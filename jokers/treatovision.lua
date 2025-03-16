86cc51978e8115ece52e3083d195830d6e17948e
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-03-16 15:53:06 -0400

    this fix has too many side effects; i'm just gonna let it be bugged if specf isn't active

--- jokers/treatovision.lua
+++ jokers/treatovision.lua
@@ -65,13 +65,5 @@ function Card:is_suit(suit, bypass_debuff, flush_calc)
             return true
         end
     end
-    if self.base.suit ~= "spectrum_fakewild" and not SMODS.has_any_suit(self) and (suit == "minty_3s" or self.base.suit == "minty_3s") then
-        if suit == "minty_3s" and not self:is_3() then
-            return false
-        end
-        if suit ~= "minty_3s" and self.base.suit == "minty_3s" then
-            return false
-        end
-    end
     return issuitref(self, suit, bypass_debuff, flush_calc)
 end
