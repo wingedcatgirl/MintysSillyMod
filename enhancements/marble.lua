deb1d10d6ad89cde6e6c5053fc1a0e84e53ed627
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-04-11 16:18:21 -0400

    marble enhancement tweak

--- enhancements/marble.lua
+++ enhancements/marble.lua
@@ -10,7 +10,9 @@ SMODS.Enhancement({
 	object_type = "Enhancement",
 	badge_colour = HEX("a3ecc0"), --Magic Mint, according to internet
     config = {
-        p_dollars = 3,
+        p_dollars = 1,
+        bonus = 15,
+        mult = 1,
         extra = {
             gymboost = "cash"
         },
@@ -19,6 +21,8 @@ SMODS.Enhancement({
         return {
             vars = {
                 card.ability.p_dollars,
+                card.ability.bonus,
+                card.ability.mult,
             },
         }
     end,
@@ -26,3 +30,12 @@ SMODS.Enhancement({
     no_rank = true,
     any_suit = true,
 })
+
+local debuffcardref = Card.set_debuff
+function Card:set_debuff(should_debuff)
+    if self.ability.name == "m_minty_marble" then
+        self.debuff = false
+        return
+    end
+        return debuffcardref(self, should_debuff)
+end
