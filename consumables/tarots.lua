93d84772664fd7f93e42f9e6d2498c9de81c29c7
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-05-22 13:38:29 -0400

    add The Bitz (summon non-legendary kity)

--- consumables/tarots.lua
+++ consumables/tarots.lua
@@ -131,6 +131,45 @@ SMODS.Consumable{ -- The Bored Child
     end
 }
 
+SMODS.Consumable {
+	object_type = "Consumable",
+	set = "Tarot",
+	name = "The Bitz",
+	key = "bitz",
+	effect = "Random Joker",
+	config = {},
+	loc_vars = function(self, info_queue, card)
+		local key = self.key
+        if MINTY.config.flavor_text then
+            key = self.key.."_flavor"
+        end
+		return {
+			key = key,
+		}
+	end,
+	cost = 4,
+	atlas = "tarots",
+	pos = { x = 2, y = 0 },
+	can_use = function(self, card)
+		if #G.jokers.cards < G.jokers.config.card_limit or self.area == G.jokers then
+			return true
+		else
+			return false
+		end
+	end,
+	use = function(self, card, area, copier)
+		G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+			play_sound('timpani')
+			local card = SMODS.add_card({
+				set = "kity",
+				area = G.jokers,
+				key_append = "minty_bitz",
+			})
+			return true end }))
+		delay(0.6)
+	end,
+}
+
 --[[SMODS.Consumable{ -- The Geologist
     set = 'Tarot', atlas = 'placeholder',
     key = 'geologist',
