e743dad3e8cb1847c6d317ba792b672ee61af614
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-08-17 20:18:04 -0400

    make pistol cards less fucky

--- modifiers/pistol.lua
+++ modifiers/pistol.lua
@@ -41,11 +41,15 @@ SMODS.Enhancement({
         end
 
         if context.minty_pistolclick and (G.STATE == G.STATES.SELECTING_HAND or G.STATE == G.STATES.DRAW_TO_HAND) then
+            if context.clicked_card ~= card then return end
             --MINTY.say("Pistol card clicked, calculating!")
             local cards = {}
+            local clicked = false
             for i = 1, #G.hand.highlighted do
                 table.insert(cards, G.hand.highlighted[i])
+                if G.hand.highlighted[i] == card then clicked = true end
             end
+            if not clicked then return end
             if #cards > 0 and SMODS.pseudorandom_probability(card, 'minty_pistolshot', 1, card.ability.extra.odds, 'minty_pistolshot') then
                 for i = 1, #cards do
                     if not cards[i].highlighted then
