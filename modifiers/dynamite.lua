e967ee287fdbf1e271e0ed5cc759243fb9c235a4
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-11-07 01:04:37 -0500

    `EXPLOSIVE`

--- modifiers/dynamite.lua
+++ modifiers/dynamite.lua
@@ -17,6 +17,26 @@ SMODS.Enhancement({
             gymboost = "random"
         }
     },
+    valk_hand_buff = {
+        title = "EXPLOSIVE",
+        colour = HEX("ab1f07"),
+        scoring_func = function (power)
+            return {
+                sound = "minty_explosion",
+                message = localize("k_minty_kaboom"),
+                func = function ()
+                    local final_chips = (G.GAME.blind.chips / 100) * (70 - power*5)
+                    final_chips = math.max(final_chips, 1)
+                    G.GAME.blind.chips = final_chips
+                    G.E_MANAGER:add_event(Event({trigger = 'after', blocking = true, func = function()
+                        G.GAME.blind:juice_up()
+                        G.GAME.blind.chip_text = number_format(final_chips)
+                        return true
+                    end}))
+                end
+            }
+        end
+    },
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
@@ -36,7 +56,8 @@ SMODS.Enhancement({
                 message = localize("k_minty_kaboom"),
                 sound = "minty_explosion",
                 func = function ()
-                    local final_chips = to_big((G.GAME.blind.chips / 100) * (100 - card.ability.extra.percent))
+                    local final_chips = (G.GAME.blind.chips / 100) * (100 - card.ability.extra.percent)
+                    final_chips = math.max(final_chips, 1)
                     G.GAME.blind.chips = final_chips
                     G.E_MANAGER:add_event(Event({trigger = 'after', blocking = true, func = function()
                         G.GAME.blind:juice_up()
